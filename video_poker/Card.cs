e8631166c6672e55c4f2eb6ca90092e80efbb4f0
emilis.masionis <emilis.masionis@gmail.com>
2019-06-07 21:47:40 +0300

    Card
    Added card class

---
+++ video_poker/Card.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace video_poker
+{
+    class Card
+    {
+        public enum SUIT
+        {
+            hearts,
+            diamonds,
+            spades,
+            clubs
+        }
+        public enum VALUE
+        {
+            two,
+            three,
+            four,
+            five,
+            six,
+            seven,
+            eight,
+            nine,
+            ten,
+            jack,
+            queen,
+            king,
+            ace
+        }
+        public SUIT Suit { get; set; }
+        public VALUE Value { get; set; }
+
+    }
+}
--- video_poker/Program.cs
+++ video_poker/Program.cs
@@ -6,7 +6,39 @@ namespace video_poker
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Video poker game");
+            while (true)
+            {
+                Console.WriteLine("Choose command:\n"
+                + "\n0: - Play the game\n"
+                + "1: - Exit\n"
+                + "Enter your choice: ");
+                try
+                {
+                    int input = Convert.ToInt32(Console.ReadLine());
+                    switch (input)
+                    {
+                        case 0:
+                            {
+                                Console.WriteLine("Play here");
+                                break;
+                            }
+                        case 1:
+                            {
+                                Environment.Exit(0);
+                                break;
+                            }
+                        default:
+                            {
+                                Console.WriteLine("Wrong input, try again");
+                                break;
+                            }
+                    }
+                }
+                catch
+                {
+                }
+            }
         }
     }
 }
