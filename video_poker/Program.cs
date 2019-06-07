860fe9c10ef927bb7efaebfa98102c765f5f95c4
emilis.masionis <emilis.masionis@gmail.com>
2019-06-07 21:45:13 +0300

    init
    init

---
+++ video_poker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace video_poker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
